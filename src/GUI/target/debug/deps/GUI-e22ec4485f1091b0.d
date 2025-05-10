917e942d81ad0b9acac84dc84d50a54fd74de1b1
Marcos Castellanos <marcos.castellanos7815@alumnos.udg.mx>
2025-05-09 21:15:48 -0600

    Commit GUI

--- src/GUI/target/debug/deps/GUI-e22ec4485f1091b0.d
+++
@@ -1,5 +0,0 @@
-/Users/marcoscastellanos/Documents/Escuela/Octavo/Programacion de sistemas avanzados/Actividades/activdad_05/sistema_defensa/src/GUI/target/debug/deps/libGUI-e22ec4485f1091b0.rmeta: src/lib.rs
-
-/Users/marcoscastellanos/Documents/Escuela/Octavo/Programacion de sistemas avanzados/Actividades/activdad_05/sistema_defensa/src/GUI/target/debug/deps/GUI-e22ec4485f1091b0.d: src/lib.rs
-
-src/lib.rs:
