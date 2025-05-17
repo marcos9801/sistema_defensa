13049e2d19cb70622629aa625f9467a967338906
Marcos Castellanos <marcos.castellanos7815@alumnos.udg.mx>
2025-05-16 19:36:00 -0600

    Creacion de plot

--- gui/src/app.rs
+++ gui/src/app.rs
@@ -10,11 +10,12 @@ use metrics::Metrics;
 
 pub struct SistemaDefensaApp {
     pub monitoring: bool,
-    pub metrics: Arc<Mutex<Metrics>>, // vector que recopila la  de los instrumentos
+    pub metrics: Arc<Mutex<Metrics>>,
     pub logs: Vec<String>,
     pub vista_actual: Vista,
-    // ...
+    pub panel_graficas: PanelGraficas,
 }
+
 enum Monitoreado {
     Activado,
     Desactivado,
@@ -32,14 +33,14 @@ impl Default for SistemaDefensaApp {
             metrics: Arc::new(Mutex::new(Metrics::new())),
             logs: Vec::new(),
             vista_actual: Vista::PanelMetricas,
+            panel_graficas: PanelGraficas::new(),
+            // panel_graficas: PanelGraficas::new(),
         }
     }
 }
 
 impl eframe::App for SistemaDefensaApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        //ctx.request_repaint_after(std::time::Duration::from_secs(1));
-
         egui::TopBottomPanel::top("menu_bar").show(ctx, |ui| {
             ui.horizontal(|ui| {
                 ui.label("Sistema de Defensa");
@@ -65,34 +66,31 @@ impl eframe::App for SistemaDefensaApp {
                         self.monitoring = false;
                         self.metrics.lock().unwrap().stop_monitoring();
                     }
-
-
                     ctx.request_repaint();
                 }
                 if ui.button("Salir").clicked() {
                     ctx.request_repaint();
                     std::process::exit(0);
                 }
-
             });
         });
 
-        match  self.vista_actual {
-                Vista::PanelMetricas => {
-                    egui::CentralPanel::default().show(ctx, |ui| {
-                        PanelMetricas::show(ui, &self.metrics);
-                    });
-                }
-                Vista::PanelAlertas => {
-                    egui::CentralPanel::default().show(ctx, |ui| {
-                        PanelAlertas::show(ui, &self.logs);
-                    });
-                }
-                Vista::PanelGraficas => {
-                    egui::CentralPanel::default().show(ctx, |ui| {
-                        PanelGraficas::show(ui);
-                    });
-                }
+        match self.vista_actual {
+            Vista::PanelMetricas => {
+                egui::CentralPanel::default().show(ctx, |ui| {
+                    PanelMetricas::show(ui, &self.metrics);
+                });
+            }
+            Vista::PanelAlertas => {
+                egui::CentralPanel::default().show(ctx, |ui| {
+                    PanelAlertas::show(ui, &self.logs);
+                });
+            }
+            Vista::PanelGraficas => {
+                egui::CentralPanel::default().show(ctx, |ui| {
+                    self.panel_graficas.show(ui, &self.metrics);
+                });
+            }
         }
     }
 }
